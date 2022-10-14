42ce11cdd62c98ce2e44ee94b733972b9ca51f92
mrKust <dimvas2010@yandex.ru>
2022-10-14 17:02:03 +0300

    fix catch bag

--- telegramPart/bot.py
+++ telegramPart/bot.py
@@ -97,18 +97,21 @@ def voice_processing(message):
             namefile = namefile.replace("\\", '')
         else:
             namefile = file_route.split(',')[1].split(':')[1][1:-2]
-   except:
-        print("Something go wrong in block of answers for voice message")
+    except:
+        print("Something go wrong in block of answers for text message")
         traceback.print_exc()
         bot.reply_to(message, "Что-то внутри меня сломалось. Так что прости, в этот раз без ответа\nНадеюсь я не ушёл в ребут")
         return
+    finally:
+        os.remove(file_name_full)
+        os.remove(file_name_full_converted)
 
-   type = responce.text
-   if type.find("poety") != -1:
+    type = responce.text
+    if type.find("poety") != -1:
         bot.reply_to(message, namefile)
-   elif type.find("quote") != -1:
+    elif type.find("quote") != -1:
         bot.reply_to(message, namefile)
-   else:
+    else:
         fileurl = ''
         for i in range(len(all_files_on_disk)):
             if all_files_on_disk[i].FIELDS['name'] == namefile:
@@ -123,11 +126,6 @@ def voice_processing(message):
             bot.send_video(chat_id=chatid, video=fileurl)
         elif type.find("mp3") != -1:
             bot.send_audio(chat_id=chatid, audio=fileurl)
-   finally:
-        os.remove(file_name_full)
-        os.remove(file_name_full_converted)
-        bot.reply_to(message, "Задушил ты меня. \n Я в ребут, так что давай до следующего раза(если это второй error message значит не в ребут)")
-
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
@@ -244,10 +242,10 @@ def text_processing(message):
         else:
             namefile = file_route.split(',')[1].split(':')[1][1:-2]
     except:
-            print("Something go wrong in block of answers for text message")
-            traceback.print_exc()
-            bot.reply_to(message, "Что-то внутри меня сломалось. Так что прости, в этот раз без ответа\nНадеюсь я не ушёл в ребут")
-            return
+        print("Something go wrong in block of answers for text message")
+        traceback.print_exc()
+        bot.reply_to(message, "Что-то внутри меня сломалось. Так что прости, в этот раз без ответа\nНадеюсь я не ушёл в ребут")
+        return
 
     type = responce.text
     if type.find("poety") != -1:
@@ -269,10 +267,6 @@ def text_processing(message):
             bot.send_video(chat_id=chatid, video=fileurl)
         elif type.find("mp3") != -1:
             bot.send_audio(chat_id=chatid, audio=fileurl)
-
-    finally:
-        bot.reply_to(message, "Задушил ты меня. \n Я в ребут, так что давай до следующего раза\n(если это второй error message значит не в ребут)")
-
 
 
 bot.polling()
